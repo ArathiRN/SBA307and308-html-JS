0b3f969906cd009fd92f65bd0cff5915c4934978
ArathiRN <arathirn@gmail.com>
2024-06-25 22:29:14 -0700

    SBA 307 and 308 html and JS

---
+++ src/main/java/com/arathin/sba307and308htmlandjs/Sba307and308HtmLandJsApplication.java
@@ -0,0 +1,13 @@
+package com.arathin.sba307and308htmlandjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sba307and308HtmLandJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sba307and308HtmLandJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arathin/sba307and308htmlandjs/Sba307and308HtmLandJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arathin.sba307and308htmlandjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sba307and308HtmLandJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
